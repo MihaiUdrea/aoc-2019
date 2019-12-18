8c347b8d0cfd5808e7ebc92b28c19eb8ac31f988
MihaiUdrea <mihaiaurelianudrea@gmail.com>
2019-12-18 15:13:22 +0200

    Update day18.cpp

--- day18.cpp
+++ day18.cpp
@@ -13,27 +13,90 @@
 //   ....X..
 //   .......
 
+struct MyStruct
+{
+  char first;
+  int second;
+};
+
+bool operator <(const MyStruct& _Left, const MyStruct& _Right)
+{
+  if (_Left.second == _Right.second)
+    return _Left.first < _Right.first;
+  return _Left.second < _Right.second;
+}
 
 //using Point = pair<int, int>;
 struct PointData
 {
-  int keysMissing;
+  //int keysMissing;
   int dist = 0;
   Point point;
-  set<char> keys;  
-  
-  auto operator<=>(const PointData&) const = default;
+  string keys;
+  set<char> doors;
+  //auto operator<=>(const PointData&) const = default;
 };
+
+struct KeyPointData
+{
+  char key;
+  int dist = 0;
+  set<char> prevKeys;
+};
+bool operator <(const KeyPointData& _Left, const KeyPointData& _Right)
+{
+  if (_Left.dist == _Right.dist)
+    return _Left.key > _Right.key;
+  return _Left.dist > _Right.dist;
+}
+
+struct KeyPointData2
+{
+  array<char,4> key;
+  int dist = 0;
+  set<char> prevKeys;
+};
+bool operator <(const KeyPointData2& _Left, const KeyPointData2& _Right)
+{
+  if (_Left.dist == _Right.dist)
+    return _Left.key > _Right.key;
+  return _Left.dist > _Right.dist;
+}
+
+struct SimplePointData
+{
+  Point point;
+  int dist = 0;
+};
+
+bool operator <(const SimplePointData& _Left, const SimplePointData& _Right)
+{
+  return _Left.dist > _Right.dist;
+}
+
+using QueKey2 = priority_queue<KeyPointData2>;
+using QueKey = priority_queue<KeyPointData>;
+
+using QueDist = priority_queue<SimplePointData>;
+
 using Que = priority_queue<PointData>;
 using Input = vector<string>;
 
-/** /
+/**/
 bool operator <(const PointData& _Left, const PointData& _Right)
 {
-  if (_Left.dist2 == _Right.dist2)
-    return _Left.dist < _Right.dist;
+  if (_Left.dist == _Right.dist)
+  {
+    if (_Left.keys.size() == _Right.keys.size())
+      if (_Left.doors.size() == _Left.doors.size())
+        return _Left.keys < _Left.keys;
+      else
+        return _Left.doors.size() < _Left.doors.size();
+    else 
+      return _Left.keys.size() < _Left.keys.size();
+  }
   else
-    return _Left.dist2 > _Left.dist2;
+    return _Left.dist > _Right.dist;
 }
 /**/
 /** /
@@ -46,22 +109,26 @@ bool operator <(const PointData& _Left, const PointData& _Right)
     }
     else
       return _Left.dist < _Right.dist;
-  else    
+  else
     return _Left.keys.size() < _Left.keys.size();
 }
 /**/
 
 array<Point, 4> delta = { Point{-1,0,0},{1,0,0},{0,-1,0},{0,1,0} };
 
+array<Point, 4> start;
+
+array<char, 4> bots = { '@', '$', '%', '*' };
+
+
 struct Solve {
   Input vec;
   Que que;
-  
+
 
   int cols = 0;
   int lines = 0;
 
-  Point start;
 
   Solve(string inStr) {
     static const regex lineSepRx("\\n");
@@ -76,11 +143,6 @@ struct Solve {
     lines = vec.size();
 
     // find start
-    for (auto line : irange(0, lines))
-    {
-      int col = vec[line].find('@');
-
-    }
 
     for (auto l : irange(0, vec.size()))
     {
@@ -88,15 +150,22 @@ struct Solve {
       for (auto c : irange(0, line.size()))
       {
         auto ch = line[c];
-        if (ch == '@')
+        int whereS = -1;
+        for (auto idx : irange(0, bots.size()))
         {
-          start = { c, l};          
-          hall.insert(start);
+          if (bots[idx] == ch)
+            whereS = idx;
+        }
+
+        if (whereS != -1)
+        {
+          start[whereS] = { c, l };
+          hall.insert({ c, l });
         }
         else if (ch >= 'A' && ch <= 'Z')
         {
           doors[{ c, l}] = ch;
-          hall.insert({c,l});
+          hall.insert({ c,l });
         }
         else if (ch >= 'a' && ch <= 'z')
         {
@@ -105,7 +174,7 @@ struct Solve {
         }
         else if (ch >= '.')
         {
-          hall.insert({ c,l});
+          hall.insert({ c,l });
         }
       }
 
@@ -130,75 +199,317 @@ struct Solve {
   set<Point> hall;
   map<Point, vector<Point>> next;
 
-  set<pair<Point, set<char>>> history;
-  string Do()
+  set<pair<Point, string>> history;
+  map<char, vector<char> > lockings;
+
+  set<Point> historySet;
+  void Parse(vector<char> & prevDoors, set<char> & doorsSet, Point pt)
   {
+
+
+    for (auto n : next[pt])
+    {
+      if (historySet.insert(n).second == false)
+        continue; // been;
+
+      if (auto whereD = doors.find(n); whereD != doors.end())
+      {
+        prevDoors.push_back(whereD->second);
+        doorsSet.insert(whereD->second);
+        Parse(prevDoors, doorsSet, n);
+        prevDoors.pop_back();
+        doorsSet.erase(whereD->second);
+      }
+      else if (auto where = keys.find(n); where != keys.end())
+      {
+        lockings[where->second] = prevDoors;
+        Parse(prevDoors, doorsSet, n);
+      }
+      else
+        Parse(prevDoors, doorsSet, n);
+    }
+  }
+
+  void Dependencies()
+  {
+    for (auto sP : start)
+    {
+      vector<char> c;
+      set<char> s;
+
+      historySet.insert(sP);
+      Parse(c, s, sP);
+
+      cout << "\nLockings:" << endl;
+
+      for (auto x : lockings)
+      {
+        cout << x.first << endl;
+        //for (auto y : x.second)
+        {
+          copy(x.second.begin(), x.second.end(),
+            ostream_iterator<char>(cout));
+          cout << endl;
+        }
+      }
+      // used gates
+      set<char> usedGates;
+      for (auto key : lockings)
+        for (auto y : key.second)
+          usedGates.insert(y);
+    }
+  }
+
+  map<pair<char, char>, int> distBetKeys;
+
+
+
+  map<char, set<MyStruct>> distFromOneKey;
   
-    que.push({ (int)keys.size(), 0, start, {} });
-    //Input mark = vec;
-    PointData best;
+  void DistancesBetweenKeys()
+  {
+    for(auto pSIdx : irange(0,start.size()))
+      keys[start[pSIdx]] = bots[pSIdx];
+
+    for(auto l : keys)
+    {
+      QueDist que;
+
+      que.push({ l.first, 0 });
+      set<Point> hist{};
+      while (!que.empty())
+      {
+        auto crEl = que.top();
+
+        que.pop();
+        /**/
+        if (hist.insert(crEl.point).second == false)
+          continue; // seen
+
+        if (auto where = keys.find(crEl.point); where != keys.end())
+        {
+          {
+            distBetKeys[{where->second, l.second}] = crEl.dist;
+            auto& x = distFromOneKey[l.second];
+            MyStruct b { where->second, crEl.dist };
+            x.insert(b);
+            int p = 0;
+            p++;
+          }
+        }
+
+        //for (auto d : delta)
+        for (auto n : next[crEl.point])
+        {
+          SimplePointData newDataPoint{ n , crEl.dist + 1 };
+
+          que.push(newDataPoint);
+        }
+      }
+
+      //cout << "D : " << distBetKeys.size();
+    }
+  }
+
+  int BFS()
+  {
+    set<pair<char, set<char>>> history;
+    QueKey que;
+    que.push({ '@', 0 });
+    
+    KeyPointData best;
+
     while (!que.empty())
     {
       auto crEl = que.top();
 
-      if (crEl.keys.size() == keys.size())
-      {
-        if (best.dist == 0 || best.dist > crEl.dist)
-        {
-          cout << "\nImproving: " << crEl.dist;
-          best = crEl;
-        }
-      }
-
       que.pop();
-      /**/
-      history.insert({ crEl.point, crEl.keys });
       
-      //for (auto d : delta)
-      for(auto n : next[crEl.point])
+      if (history.insert({ crEl.key, crEl.prevKeys }).second == false)
+        continue; // seen
+
+      crEl.prevKeys.insert(crEl.key);
+
+      if (crEl.prevKeys.size() == keys.size())
+        return crEl.dist;
+
+      if (crEl.prevKeys.size() > best.prevKeys.size())
+        best = crEl;
+
+      auto& list = distFromOneKey[crEl.key];
+      for (auto n : list)
       {
-        PointData newDataPoint{ 0, crEl.dist + 1, n , crEl.keys};
+        if (n.first == crEl.key)
+          continue; // skip self
 
-        if (newDataPoint.dist >= 4502)
+        if (crEl.prevKeys.find(n.first) != crEl.prevKeys.end())
+          continue; // already been to this key
+
+        KeyPointData newDataPoint{ n.first, crEl.dist + n.second, crEl.prevKeys};
+
+        // verify all keys are present
+        bool bad = false;
+        for (auto key : lockings[n.first])
+        {
+          if (newDataPoint.prevKeys.find(tolower(key)) == newDataPoint.prevKeys.end())
+          {
+            bad = true;
+            break;
+          }
+        }
+        if (bad)
           continue;
-
-        if (auto where = keys.find(newDataPoint.point); where != keys.end())
-        {
-          newDataPoint.keys.insert(where->second);
-        }
-        else if (auto whereD = doors.find(newDataPoint.point); whereD != doors.end())
-        {
-          auto neededKey = tolower(whereD->second);
-          if (newDataPoint.keys.find(neededKey) == newDataPoint.keys.end())
-            continue; // no key
-        }
-        else if (hall.find(newDataPoint.point) == hall.end())
-        {
-          continue; // wall
-        }
-        else if (history.find({ newDataPoint.point, newDataPoint.keys }) != history.end())
-        {
-          continue; // visited
-        }
-
-        newDataPoint.keysMissing = newDataPoint.keys.size();
-
+        
         que.push(newDataPoint);
       }
-
-      /**/
     }
+    return -1;
+  }
 
-    return to_string(best.dist);
+  int BFS2()
+  {
+    set<pair<array<char,4>, set<char>>> history;
+    QueKey2 que;
+    que.push({ bots, 0 });
+
+    KeyPointData2 best;
+
+    while (!que.empty())
+    {
+      auto crEl = que.top();
+
+      que.pop();
+
+      if (history.insert({ crEl.key, crEl.prevKeys }).second == false)
+        continue; // seen
+
+      for(auto x : crEl.key)
+        crEl.prevKeys.insert(x);
+
+      if (crEl.prevKeys.size() == keys.size())
+        return crEl.dist;
+
+      if (crEl.prevKeys.size() > best.prevKeys.size())
+        best = crEl;
+
+      for (auto xi : irange(0,crEl.key.size()))
+      {
+        auto x = crEl.key[xi];
+
+        auto& list = distFromOneKey[x];
+        for (auto n : list)
+        {
+          if (n.first == x)
+            continue; // skip self
+
+          if (crEl.prevKeys.find(n.first) != crEl.prevKeys.end())
+            continue; // already been to this key
+
+          KeyPointData2 newDataPoint{ crEl.key, crEl.dist + n.second, crEl.prevKeys };
+          newDataPoint.key[xi] = n.first;
+
+          // verify all keys are present
+          bool bad = false;
+          for (auto key : lockings[n.first])
+          {
+            if (newDataPoint.prevKeys.find(tolower(key)) == newDataPoint.prevKeys.end())
+            {
+              bad = true;
+              break;
+            }
+          }
+          if (bad)
+            continue;
+
+          que.push(newDataPoint);
+        }
+      }
+    }
+    return -1;
+  }
+
+  string Do()
+  {
+    Dependencies();
+
+    DistancesBetweenKeys();
+
+    //auto res = BFS();
+    auto res = BFS2();
+    toClipboard(res);
+    return to_string(res); 
   }
 };
 
 TEST_CASE("Sample 1", "[.]") {
   cout << endl << "Tests   ------------- " << endl;
 
-  //  REQUIRE(Solve("1,9,10,3,2,3,11,0,99,30,40,50").Do() == "3500"); // sample test
-  REQUIRE(Solve(ReadFileToString(L"sample/sample.txt")).Do() == ReadFileToString(L"sample/result.txt"));
+  REQUIRE(Solve(ReadFileToString(L"sample/sample4.txt")).Do() == ReadFileToString(L"sample/result4.txt"));
+
+  REQUIRE(Solve(ReadFileToString(L"sample/sample3.txt")).Do() == ReadFileToString(L"sample/result3.txt"));
+
+
+  CHECK(Solve(R"(
+########################
+#f.D.E.e.C.b.A.@.a.B.c.#
+######################.#
+#d.....................#
+########################
+)").Do() == "86");
+
+  REQUIRE(Solve(R"(
+#########
+#b.A.@.a#
+#########)").Do() == "8");
   REQUIRE(Solve(ReadFileToString(L"sample/sample2.txt")).Do() == ReadFileToString(L"sample/result2.txt"));
+
+}
+
+TEST_CASE("Sample 2", "[x.]") {
+  cout << endl << "Tests   ------------- " << endl;
+
+  CHECK(Solve(R"(
+#######
+#a.#Cd#
+##@#$##
+#######
+##*#%##
+#cB#.b#
+#######
+)").Do() == "8");
+
+  REQUIRE(Solve(R"(
+###############
+#d.ABC.#.....a#
+######@#$######
+###############
+######*#%######
+#b.....#.....c#
+###############
+)").Do() == "24");
+
+  REQUIRE(Solve(R"(
+#############
+#DcBa.#.GhKl#
+#.###@#$#I###
+#e#d#####j#k#
+###C#*#%###J#
+#fEbA.#.FgHi#
+#############
+)").Do() == "32");
+
+  REQUIRE(Solve(R"(
+#############
+#g#f.D#..h#l#
+#F###e#E###.#
+#dCba@#$BcIJ#
+#############
+#nK.L*#%G...#
+#M###N#H###.#
+#o#m..#i#jk.#
+#############
+)").Do() == "72");
+  
 }
 
 TEST_CASE("Part One", "[x.]") {
@@ -212,4 +523,3 @@ TEST_CASE("Part Two", "[.]") {
 
   toClipboard(Solve(ReadFileToString(L"input.txt")).Do());
 }
-
